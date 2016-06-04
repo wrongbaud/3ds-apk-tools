de8630e1a480e3f655d7d63dbb37c9fe279165ea
altrom <altrom.contact@gmail.com>
2016-06-03 23:26:28 -0400

    Initial commit, just adding the basic files needed

---
+++ README.md
@@ -0,0 +1,49 @@
+#apk-tools
+----
+Tools for unpacking and repacking APK files found on various 3DS roms
+
+---
+## Usage:
+
+* See ```apk.py``` for class definitions and example usage
+* ```apk-template.bt``` is an 010 template for apk files that I made to assist in reverse engineering the format 
+
+----
+## Building a RomFS
+
+See the following forum post for a decent tutorial on how to extract and rebuild a RomFS:
+
+* https://gbatemp.net/threads/tutorial-how-to-decrypt-extract-rebuild-3ds-roms.383055/
+
+----
+##Gundam: The 3D Battle - Project notes and such...
+
+* We are currently able to exctract textures from apk files, modify them, and then use these modified files using HANS.
+
+* Since this is a JP cart however, and HANS dosen't support all regions we must do the following to get it to work.
+
+1. Modify textures and recompress them into apk files, build the new RomFS
+    * See Building a RomFS for more details
+2. Start 3DS in gateway mode (requires flash cart)
+    * Once in gateway mode, select the Gundam ROM on the flash cart
+3. Enter homebrew launcher (either through  homebrew cia or other input vector)
+4. Run HANS and redirect the RomFS transactions to your modified RomFS
+
+This is not very efficient for testing (and requires a flash cart) so we may be able to use the LayerFS plugin capability of the NTR-CFW. Unfortunately though for this particular ROM, the toolkit to build the LaterFS plugin could not find a few of the symbols needed to perform the proper hooking to redirect RomFS access. This is something that we can probably find though. The layerFS build tools are included in this repo as well.
+
+This issue was explained to me through someone on GBATemp (See link below):
+
+* https://gbatemp.net/threads/ntr-cfw-layeredfs-freezing-at-launch-screen-for-gundam-the-3d-battle.424793/
+
+----
+## LayeredFS
+
+LayeredFS is a plugin for NTR CFW that allows users to swap out individual files on their romFS with ones placed on the SD card, this is really useful for testing mods and would be beneficial to our long term workflow. The only problem is that it does not find all of the proper symbols in our Gundam rom in order to provide the necessary hooks to the CFW. The next step for us (if we want to employ this method) is to find the necessarry symbols for the ROM and enter them manually. The codebase is relatively straightforward and should work on a Linux host (I have modified the build scripts to work with a Linux system as opposed to it's original design which was Windows...
+
+---
+##Helpful Links for future work:
+* https://gbatemp.net/threads/how-to-create-and-use-a-layeredfs-plugin.389977/
+* https://gbatemp.net/threads/release-ntr-cfw-3-2-with-experimental-real-time-save-feature.385142/
+* https://github.com/44670/layeredFS/wiki/manual
+* https://github.com/44670/BootNTR/releases
+* https://gbatemp.net/threads/release-3ds-simple-cia-converter.384559/
---
+++ apk.py
@@ -0,0 +1,328 @@
+#!/usr/bin/python 
+import shutil
+import zlib
+import time
+import os
+import sys
+import logging
+import struct
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+hdlr = logging.FileHandler('guntanked.log')
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+hdlr.setFormatter(formatter)
+logger.addHandler(hdlr) 
+
+class apk_hdr:
+
+    def __init__(self):
+        self.offset = 0
+        self.desc = ''
+        self.length = 0
+        self.unknown_val_1 = 0
+        self.data = []
+
+class pack_hdr:
+
+    def __init__(self):
+        self.offset = 0
+        self.desc = ''
+        self.length = 0
+        self.first_file_offset = 0
+        self.unknown_vals = []
+        self.checksum_vals = []
+
+class packtoc_hdr:
+    
+    def __init__(self):
+        self.offset = 0
+        self.desc = ''
+        self.object_size = 0
+        self.length = 0
+        self.object_count = 0
+        self.unknown = []
+        self.entries = []
+
+class packtoc_entry:
+
+    def __init__(self):
+        self.offset = 0
+        self.id = 0
+        self.count = 0
+        self.unknown = []
+        self.file_offset = 0
+        self.decompressed_len = 0
+        self.compressed_len = 0
+        self.name = ""
+        self.unknown_element = 0
+
+    def __str__(self):
+        return "ID: 0x%x Count: 0x%x File Offset = 0x%x Decompressed Len: 0x%x Compressed Len: 0x%x Unknown: 0x%x" % (self.id,self.count,self.file_offset,self.decompressed_len,self.compressed_len,self.unknown_element)
+
+class packfsls_hdr:
+
+    def __init__(self):
+        self.offset = 0
+        self.desc = ''
+        self.length = 0
+        self.unknown = []
+
+class genestr_hdr:
+
+    def __init__(self):
+        self.offset = 0
+        self.desc = ''
+        self.length = 0 
+        self.unknown = []
+        self.str_table_offset = 0
+        self.length_2 = 0
+        self.object_count = 0
+        self.str_offsets_loc = 0
+        self.str_offsets = []
+        self.entries = []
+
+class apk_file:
+
+    def __init__(self,filepath):
+        self.filepath = filepath
+        self.apk = open(filepath,'rb')
+        self.genestr_hdr = None      #This section holds the strings and table of offsets into the string table for naming files
+        self.packfsls_hdr = None     #This section is empty for most of the archives, assuming it means filessystem something
+        self.apk_hdr = None             #This section seems to be at the beginning of each file
+        self.pack_hdr = None            #This section is the generic header for the "PACK" sections (I think)
+        self.packtoc_hdr = None         #This secrion describes the PACK Table of Contents which defines all file entries
+        self.packtoc_entries = []       #This is a list of objects representing the PACKTOC entries
+
+    #Check the APK initial header, bail if not ENDILTLE
+    def _check_apk_hdr(self,offset=None):
+        if offset != None:
+            self.apk.seek(offset)
+        else:
+            self.apk.seek(0)
+        endiltle = self.apk.read(8)
+        if endiltle.decode("utf-8") != 'ENDILTLE':
+            logger.info("First header ENDILTLE not found, exiting!")
+            self.apk.close()
+            return 0
+        else:
+            self.apk_hdr = apk_hdr()
+            self.apk_hdr.offset = 0
+            self.apk_hdr.desc = endiltle.decode("utf-8")
+            self.apk_hdr.length = int(struct.unpack("I",self.apk.read(4))[0])
+            logger.info("Found ENDILTLE header for file: %s at offset 0x%x" % (self.filepath,self.apk_hdr.offset))
+            return 1
+
+    #Check the PACKHEDR portion
+    def _check_pack_hdr(self,offset=None):
+        self.apk.seek(0)
+        if offset == None:
+            offset = 0x10 #Note: This may not always be true, it's just what I've observed!
+        self.apk.seek(offset)
+        self.pack_hdr = pack_hdr()
+        self.pack_hdr.desc = self.apk.read(8).decode("utf-8")
+        if self.pack_hdr.desc != "PACKHEDR":
+            logger.info("PACKHEDR not found in file: %s exiting now" % self.filename)
+            return 0
+        else:
+            self.pack_hdr.offset = offset
+            self.pack_hdr.length = int(struct.unpack("I",self.apk.read(4))[0])
+            for x in range(0,3):
+                self.pack_hdr.unknown_vals.append(struct.unpack("I",self.apk.read(4))[0])
+            self.pack_hdr.first_file_offset = int(struct.unpack("I",self.apk.read(4))[0])
+            self.pack_hdr.unknown_vals.append(struct.unpack("I",self.apk.read(4)))
+            for x in range(0,4):
+                self.pack_hdr.checksum_vals.append(struct.unpack("I",self.apk.read(4))[0])
+            logger.info("Found PACKHEDR Section for file: %s at offset 0x%x" % (self.filepath,self.pack_hdr.offset))
+            return 1
+
+    def _check_packtoc_hdr(self,offset=None):
+        self.apk.seek(0)
+        if offset == None:
+            offset = 0x40
+        self.apk.seek(offset)
+        self.packtoc_hdr = packtoc_hdr()
+        self.packtoc_hdr.offset = offset
+        self.packtoc_hdr.desc = self.apk.read(8).decode("utf-8")
+        if self.packtoc_hdr.desc != "PACKTOC ":
+            logger.info("PACKTOC not found in file: %s, exiting now" % self.filename)
+            return 0
+        else:
+            self.packtoc_hdr.length = int(struct.unpack("I",self.apk.read(4))[0])
+            self.packtoc_hdr.unknown.append(struct.unpack("I",self.apk.read(4)))
+            self.packtoc_hdr.object_size = int(struct.unpack("I",self.apk.read(4))[0])
+            self.packtoc_hdr.object_count = int(struct.unpack("I",self.apk.read(4))[0])
+            self.packtoc_hdr.unknown.append(int(struct.unpack("I",self.apk.read(4))[0]))
+            self.packtoc_hdr.unknown.append(int(struct.unpack("I",self.apk.read(4))[0]))
+            logger.info("Found PACTOC Section for file: %s at offset 0x%x" % (self.filepath,self.packtoc_hdr.offset))
+            logger.info("\tPACKTOC Object Count: 0x%x" % self.packtoc_hdr.object_count)
+            logger.info("\tPACKTOC Object Size: 0x%x" % self.packtoc_hdr.object_size)
+            return 1
+
+    def _build_packtoc_objects(self,offset=None):
+        self.apk.seek(0)
+        if offset == None:
+            offset = 0x60   #Again, this is just what I have seen, could certainly be different in other files
+        self.apk.seek(offset)
+        for x in range(0,self.packtoc_hdr.object_count):
+            obj = packtoc_entry()
+            obj.offset = self.apk.tell()
+            obj.id = int(struct.unpack("I",self.apk.read(4))[0])
+            obj.count = int(struct.unpack("I",self.apk.read(4))[0])
+            obj.unknown.append(struct.unpack("I",self.apk.read(4)))
+            obj.unknown.append(struct.unpack("I",self.apk.read(4)))
+            obj.file_offset = int(struct.unpack("I",self.apk.read(4))[0])
+            obj.unknown.append(struct.unpack("I",self.apk.read(4)))
+            obj.decompressed_len = int(struct.unpack("I",self.apk.read(4))[0])
+            obj.unknown.append(struct.unpack("I",self.apk.read(4)))
+            obj.compressed_len = int(struct.unpack("I",self.apk.read(4))[0])
+            obj.unknown.append(struct.unpack("I",self.apk.read(4)))
+            self.packtoc_hdr.entries.append(obj)
+        logger.info("\tPACKTOC Objects Parsed: 0x%x" % len(self.packtoc_hdr.entries))
+    
+    #Don't really know much about this structure to confirm anything...    
+    def _check_packfsls_hdr(self,offset=None):
+        self.apk.seek(0)
+        if offset == None:
+            offset = self.packtoc_hdr.offset + self.packtoc_hdr.length + 0x10
+            #Make sure that it's 0x10 byte alligned...
+            while offset % 0x10 != 0:
+                offset += 1
+        self.apk.seek(offset)
+        self.packfsls_hdr = packfsls_hdr()
+        self.packfsls_hdr.offset = offset
+        self.packfsls_hdr.desc = self.apk.read(8).decode("utf-8")
+        if self.packfsls_hdr.desc != "PACKFSLS":
+            logger.info("PACKFSLS not found in file %s at offset 0x%x, exiting now" % (self.filepath,offset)) 
+            return 0
+        else:
+            self.packfsls_hdr.length = int(struct.unpack("I",self.apk.read(4))[0])
+            logger.info("Found PACKFSLS Section for file %s at offset 0x%x" % (self.filepath,self.packfsls_hdr.offset))
+            return 1
+
+    def _check_genestr_hdr(self,offset=None):
+        self.apk.seek(0)
+        if offset == None:
+            offset = self.packfsls_hdr.offset + self.packfsls_hdr.length + 0x10
+            while offset % 0x10 != 0:
+                offset += 1
+        self.apk.seek(offset)
+        self.genestr_hdr = genestr_hdr()
+        self.genestr_hdr.offset = offset
+        self.genestr_hdr.desc = self.apk.read(8).decode("utf-8")
+        self.genestr_hdr.str_offset_loc = 0x10  #Not entire sure about this, will need to see more files first...
+        if self.genestr_hdr.desc != "GENESTRT":
+            logger.info("GENESTRT not found in file %s at offset 0x%x" % (self.filepath,self.genestr_hdr.offset))
+            return 0
+        else:
+            self.genestr_hdr.length = int(struct.unpack("I",self.apk.read(4))[0])
+            self.genestr_hdr.unknown.append(struct.unpack("I",self.apk.read(4)))
+            self.genestr_hdr.object_count = int(struct.unpack("I",self.apk.read(4))[0])
+            self.genestr_hdr.unknown.append(struct.unpack("I",self.apk.read(4)))
+            self.genestr_hdr.str_table_offset = int(struct.unpack("I",self.apk.read(4))[0])
+            self.genestr_hdr.length_2 = int(struct.unpack("I",self.apk.read(4))[0])
+            for x in range(0,self.genestr_hdr.object_count):
+                self.genestr_hdr.str_offsets.append(int(struct.unpack("I",self.apk.read(4))[0]))
+            logger.info("Found GENESTRT Section for file %s at offset 0x%x" % (self.filepath, self.genestr_hdr.offset))
+            logger.info("\tGENESTRT offsets parsed: 0x%x" % len(self.genestr_hdr.str_offsets))
+
+    def _apply_names_to_objects(self):
+        for obj in self.packtoc_hdr.entries:
+            obj.name = self.genestr_hdr.entries[obj.count]
+
+    def _build_genestrt_objects(self,offset=None):
+            self.apk.seek(0)
+            if offset == None:
+                offset = self.genestr_hdr.offset + self.genestr_hdr.str_table_offset + 0x10
+            logger.info("Attempting to read GENESTRT objects at offset 0x%x" % offset) 
+            self.apk.seek(offset)
+            total_bytes = self.genestr_hdr.length
+            str_count = self.genestr_hdr.object_count
+            while str_count > 0:
+                val = self.apk.read(1)
+                new_str = ''
+                while val != b"\x00":
+                    new_str += val.decode("utf-8")
+                    val = self.apk.read(1)
+                str_count -=1
+                if new_str == "GENEEOF ":
+                    logger.info("\tFinished parsing filename strings!")
+                    break
+                self.genestr_hdr.entries.append(new_str)
+ 
+    def _export_apk(self,output_dir):
+        logger.info("Attempting to export %s to directory %s" % (self.filepath,output_dir))
+        output_dir += '/'
+        for obj in self.packtoc_hdr.entries:
+            self.apk.seek(0)
+            if obj.id == 0x200:
+                start_addr = obj.file_offset
+                length = obj.compressed_len
+                self.apk.seek(start_addr)
+                buf = self.apk.read(length)
+                new_file = zlib.decompress(buf)
+                try:
+                    out = open(output_dir + self.genestr_hdr.entries[obj.count],'wb+')
+                    out.write(new_file)
+                    out.close()
+                except IsADirectoryError as e:
+                    out = open(output_dir+"UNKNOWN-%f" % time.time(),'wb+')
+                    out.write(new_file)
+                    out.close()
+                except FileNotFoundError as e:
+                    logger.info("Error attempting to export data, file not found -- does the specified output directory exist?")
+                    return 0
+        logger.info("Finished exporting APK Files")
+
+    ###NOTE: NOT TESTED -- I DO NOT RECCOMEND CURRENTLY FUCKING WITH THIS
+    ### THIS ASSUMES THAT THE NEW FILE HAS THE SAME OR LESS COMPRESSED SIZE!
+    def _import_apk(self,current_file,new_file):
+        if self.apk != None:
+            self.apk.close()
+        logger.info("Making modified copy of %s please wait..." % self.filepath)
+        os.copy(self.filename,'modded'+self.filepath)
+        self.apk = open('modded'+self.filepath,'rb+')
+        #Import and compress our new texture
+        updated_file = open(new_file,'rb').read()
+        d_updated_size = len(updated_file)
+        z_updated_file = zlib.compress(updated_file)
+        z_updated_size = len(z_updated_file)
+        #Get info on old texture
+        target_element == None
+        for element in self.packtoc_entries:
+            if element.name == current_file:
+                logger.info("Found target element %s in PACKTOC entries!")
+                logger.info("\t%s" % element)
+                target_element = element
+        #Update attributes based on our new texture...
+        if z_updated_size != target_element.compressed_len:
+            target_element.compressed_len = z_updated_size
+        if d_updated_size != target_element.decompressed_len:
+            target_element.compressed_len = d_updated_size
+        logger.info("New Element:\n")
+        logger.info("\t%s" % target_element)
+        #Write our new data
+        #TODO: IF WE ARE LARGER THAN THE ORIGINAL WE NEED TO MODIFY ALL OF THE TABLE OFFSET AFTER THIS ONE
+        self.apk.seek(target_element.file_offset)
+        self.apk.write(z_updated_file)
+        #Modify the table entry
+        self.apk.seek(0)
+        self.apk.seek(target_element.offset + 24)
+        self.apk.write(struct.pack("I",d_updated_size))
+        self.apk.seek(4)
+        self.apk.write(struct.pack("I",z_updated_size))
+        self.apk.close()
+ 
+if __name__ == "__main__":
+    test_ap = apk_file(sys.argv[1])
+    test_ap._check_apk_hdr()
+    test_ap._check_pack_hdr()
+    test_ap._check_packtoc_hdr()
+    test_ap._build_packtoc_objects()
+    test_ap._check_packfsls_hdr()
+    test_ap._check_genestr_hdr()
+    test_ap._build_genestrt_objects()
+    test_ap._apply_names_to_objects()
+    test_ap._import_apk("d_04.bctex","d_04.bctex")
+    #test_ap._export_apk(sys.argv[2])
+    
